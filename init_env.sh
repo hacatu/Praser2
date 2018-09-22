1a9b8612433313a613a400d1745c493662f3fb15
Gabriel Eiseman <hacatu5000@gmail.com>
2018-09-22 02:04:13 -0400

    Removed old build and test scripts

--- init_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export BUILD_ROOT=`pwd`
-export GCOV_PREFIX=$BUILD_ROOT/notes
-export GCOV_PREFIX_STRIP=$((`echo $GCOV_PREFIX|tr -cd '/' | wc -c`*2+6))
-echo CONFIG_BUILD_ROOT=$BUILD_ROOT > tup.config
-
